7b51620f19386f3f23aefa13d6f3e7b92097dda3
sudgr <122168129+sudgr@users.noreply.github.com>
2023-06-26 16:12:20 +0700

    props to component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
 // import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+// import ClassCounter from './components/ClassCounter';
+import './styles/App.css'
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/Ui/button/MyButton';
 
 function App() {
-  // const [value, setValue] = useState('Текст в инпуте')
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javacript', body: 'Description'},
+    {id: 2, title: 'Vue', body: 'Non-comerse'},
+    {id: 3, title: 'Vue2', body: 'Non-comerse2'},
+
+  ])
 
 
 
   return (
     <div className="App">
-      {/* <Counter/> */}
-      <ClassCounter></ClassCounter>
+      <form>
+        <input type="text" placeholder='The Name Post'/>
+        <input type="text" placeholder='Description Post'/>
+        <MyButton>Create Post</MyButton>
+      </form>
+     <PostList posts={posts} title="Список постов 1"/>
     </div>
   );
 }
