2a3145e048f2bbc2a1421e8607404c37f8be1401
sudgr <122168129+sudgr@users.noreply.github.com>
2023-06-27 02:12:02 +0700

    on loading

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,33 +1,35 @@
-import React, { useState, useRef, useMemo } from 'react'
-// import Counter from './components/Counter';
-// import ClassCounter from './components/ClassCounter';
+import React, { useState, useEffect } from 'react'
 import './styles/App.css'
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/Ui/button/MyButton';
-import MyInput from './components/Ui/input/MyInput';
 import PostForm from './components/PostForm';
-import MySelect from './components/Ui/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/Ui/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
+import PostService from './API/PostService';
+
 
 function App() {
-
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Javacript', body: '22' },
-    { id: 2, title: 'wew', body: '23n-comerse' },
-    { id: 3, title: 'sfsdfsde2', body: '76fsdfrse2' },
-
-  ])
-
- const [filter, setFilter] = useState({sort: '', query: ''})
+  const [posts, setPosts] = useState([])
+  const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState('')
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+  const [isPostsLoading, setPostLoading] = useState('')
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
+  }
+
+  async function fetchPosts() {
+    setPostLoading(true)
+    const posts = await PostService.getAll()
+    setPosts([...posts])
+    setPostLoading(false)
   }
 
   const removePost = (post) => {
@@ -36,15 +38,19 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+      <MyButton onClick={fetchPosts}>Get posts</MyButton>
+      <MyButton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
         Create Post
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
-      <PostForm create={createPost} />
+        <PostForm create={createPost} />
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
-      <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов" />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {isPostsLoading
+        ? <h1>Идет загрузка...</h1>
+        : <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов" />}
+
     </div>
   );
 }
